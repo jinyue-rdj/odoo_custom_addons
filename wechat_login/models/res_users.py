aa9b78abb906147b70dd958ede3acad66fd4b8be
odoo <guohuanhuan_cool@126.com>
2018-04-22 13:41:38 +0800

    correct some errors

--- wechat_login/models/res_users.py
+++ wechat_login/models/res_users.py
@@ -3,6 +3,7 @@ import werkzeug
 import json
 from urllib.parse import urlparse
 from urllib import request
+import requests
 import logging
 
 from odoo import models, fields, api
@@ -16,18 +17,16 @@ class ResUsers(models.Model):
 
     def _auth_oauth_wechat_rpc(self, client_id, validation_endpoint, data_endpoint, code, secret):
         params = werkzeug.url_encode({'code': code, 'appid': client_id, 'secret': secret, 'grant_type': 'authorization_code'})
-        url = validation_endpoint + '?' + params
-        _logger.info('Request %s to get access_token'%url)
-        f = request.urlopen(url)
-        response = f.read()
-        validation = json.loads(response)
+        validation = requests.get(validation_endpoint, params=params).json()
+
+        if validation.get("errmsg"):
+            raise Exception("validation_endpoint error "+ validation['errmsg'])
         if 'access_token' in validation:
             params = werkzeug.url_encode({'access_token': validation['access_token'], 'openid': validation['openid']})
-            url = data_endpoint + '?' + params
-            logger.info('Request %s to get userinfo'%url)
-            f = request.urlopen(url)
-            response = f.read()
-            validation = json.loads(response)
+            validation = requests.get(data_endpoint, params=params).json()
+
+            if validation.get("errmsg"):
+                raise Exception("data_endpoint error "+validation['errmsg'])
             if 'unionid' in validation:
                 validation['user_id'] = validation['unionid']
             if 'nickname' in validation:
@@ -37,11 +36,12 @@ class ResUsers(models.Model):
     @api.model
     def _auth_oauth_validate(self, provider, access_token):
         """ return the validation data corresponding to the access token """
-        wechat_provider_id = request.env.ref("wechat_login.provider_wechat").id
+        wechat_provider_id = self.env.ref("wechat_login.provider_wechat").id
         oauth_provider = self.env['auth.oauth.provider'].browse(provider)
+
         if oauth_provider.id == wechat_provider_id:
-            ir_config_wechat_id = request.env['ir.config_parameter'].sudo().search([('key', '=', 'wechat_login.secret')])
-            wechat_secret = request.env['ir.config_parameter'].sudo().browse(int(ir_config_wechat_id))[0].value
+            ir_config_wechat_id = self.env['ir.config_parameter'].sudo().search([('key', '=', 'wechat_login.secret')])
+            wechat_secret = self.env['ir.config_parameter'].sudo().browse(int(ir_config_wechat_id))[0].value
             validation = self._auth_oauth_wechat_rpc(oauth_provider.client_id, oauth_provider.validation_endpoint, oauth_provider.data_endpoint, access_token ,wechat_secret)
             return validation
         else:
