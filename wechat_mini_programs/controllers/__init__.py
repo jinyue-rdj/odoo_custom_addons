31dce9145a9741d7116c8e2653974cb0bb49bb2a
odoo <guohuanhuan_cool@126.com>
2018-08-24 14:08:47 +0800

    add splash controller

--- wechat_mini_programs/controllers/__init__.py
+++ wechat_mini_programs/controllers/__init__.py
@@ -5,3 +5,4 @@ from . import wechat_category_controller
 from . import wechat_transfer_controller
 from . import wechat_english_controller
 from . import wechat_feedback_controller
+from . import wechat_splash_controller
