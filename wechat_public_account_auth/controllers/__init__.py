a14b966f81ee5a8a8970fdd3738fc10de63ebc1a
odoo <guohuanhuan_cool@126.com>
2018-10-12 20:34:54 +0800

    commit validate code

--- wechat_public_account_auth/controllers/__init__.py
+++ wechat_public_account_auth/controllers/__init__.py
@@ -1,6 +1,2 @@
-# -*- coding: utf-8 -*-
-# Copyright (C) 2018 MAXSNS Corp (http://www.maxsns.com)
-# @author Henry Zhou (zhouhenry@live.com)
-# License OPL-1 - See https://www.odoo.com/documentation/user/11.0/legal/licenses/licenses.html
 
-from . import wechat_mini_program_auth
+from . import wechat_public_account_auth
--- wechat_public_account_auth/controllers/wechat_public_account_auth.py
+++ wechat_public_account_auth/controllers/wechat_public_account_auth.py
@@ -1,8 +1,6 @@
-# -*- coding: utf-8 -*-
-# Copyright (C) 2018 MAXSNS Corp (http://www.maxsns.com)
-# @author Henry Zhou (zhouhenry@live.com)
-# License OPL-1 - See https://www.odoo.com/documentation/user/11.0/legal/licenses/licenses.html
 
+import hashlib
+import six
 import werkzeug
 import requests
 from odoo import http, tools
@@ -11,141 +9,50 @@ from odoo.service import security
 import logging
 
 _logger = logging.getLogger(__name__)
-target_url = "https://api.weixin.qq.com/sns/jscode2session"
 
 
-def ensure_db():
-    db = request.params.get('db') and request.params.get('db').strip()
+class WeChatPublicAccountAuth(http.Controller):
 
-    # Ensure db is legit
-    if db and db not in http.db_filter([db]):
-        db = None
-
-    if db and not request.session.db:
-        # User asked a specific database on a new session.
-        request.session.db = db
-        return
-
-    # if db not provided, use the session one
-    if not db and request.session.db and http.db_filter([request.session.db]):
-        db = request.session.db
-
-    # if no database provided and no database in session, use monodb
-    if not db:
-        db = http.db_monodb(request.httprequest)
-
-    # always switch the session to the computed db
-    if db != request.session.db:
-        request.session.logout()
-
-    request.session.db = db
-
-
-def _get_open_id(code, appid):
-    secret = request.env['ir.config_parameter'].sudo().get_param('wechat.mp.secret.' + appid, '')
-    if not secret:
-        raise RuntimeError('Cannot get App Secret parameter for ' + appid)
-    params = werkzeug.url_encode({
-        "appid": appid, "secret": secret, "js_code": code, "grant_type": "authorization_code"
-    })
-    result = requests.get(target_url, params=params).json()
-    if "openid" in result and result["openid"]:
-        return result["openid"]
-    elif "errcode" in result:
-        raise RuntimeError(('WeChat Server Error: %s ' + result["errmsg"]) % result["errcode"])
-    else:
-        raise RuntimeError('Unknown WeChat Server Error')
-
-
-def _set_session_info(uid, login):
-    request.session.uid = uid
-    request.session.login = login
-    request.session.session_token = security.compute_session_token(request.session, request.env)
-    request.uid = uid
-    request.disable_db = False
-    request.session.get_context()
-
-
-def _get_session_info():
-    return request.env["ir.http"].sudo().session_info()
-
-
-class WeChatMiniProgramAuth(http.Controller):
-
-    @http.route('/wechat_mp_auth/logout', type='json', auth="user")
-    def logout(self, **kw):
-        request.session.logout(keep_db=True)
-        return True
-
-    @http.route('/wechat_mp_auth/get_uid', type='json', auth="user", methods=["POST"])
-    def get_uid(self, **kw):
-        return {"uid": request.uid}
-
-    @http.route('/wechat_mp_auth/auth_by_login', type='json', auth="none", methods=["POST"])
-    def auth_by_login_third_part(self, code, appid, login, pwd, **kw):
-        ensure_db()
-        if login and pwd:
-            account = request.env['wechat.mini.program.account'].sudo()
-            '''try:
-                uid = request.session.authenticate(request.session.db, login, pwd)
-            except RuntimeError as ex:'''
-            uid = account.check_third_part_login(login, pwd)
-            if uid:
-                try:
-                    openid = _get_open_id(code, appid)
-                except RuntimeError as e:
-                    return {'error': 'wechat_error', 'message': '%s' % e}
-                account.update_or_create_wechat_account_user(openid, appid, uid)
-                _set_session_info(uid, login)
-                return {'success': 'You have been logged in successfully!', 'session': _get_session_info()}
-        return {'error': 'invalid_login', 'message': 'Invalid login name or password.'}
-
-    @http.route('/wechat_mp_auth/auth_by_login_odoo', type='json', auth="none", methods=["POST"])
-    def auth_by_login_odoo_part(self, code, appid, login, pwd, **kw):
-        ensure_db()
-        if login and pwd:
-            account = request.env['wechat.mini.program.account'].sudo()
-            try:
-                uid = request.session.authenticate(request.session.db, login, pwd)
-            except Exception as ex:
-                info = tools.ustr(ex)
-            if uid:
-                try:
-                    openid = _get_open_id(code, appid)
-                except RuntimeError as e:
-                    return {'error': 'wechat_error', 'message': '%s' % e}
-                account.update_or_create_wechat_account_user(openid, appid, uid)
-                _set_session_info(uid, login)
-                return {'success': 'You have been logged in successfully!', 'session': _get_session_info()}
-        return {'error': 'invalid_login', 'message': 'Invalid login name or password.'}
-
-    @http.route('/wechat_mp_auth/auth_by_code', type='json', auth="none", methods=["POST"])
-    def auth_by_code(self, code, appid, **kw):
-        ensure_db()
-        try:
-            openid = _get_open_id(code, appid)
-        except RuntimeError as e:
-            return {'error': 'wechat_error', 'message': '%s' % e}
-        account = request.env['wechat.mini.program.account'].sudo().search(
-            [('wechat_open_id', '=', openid), ('wechat_app_id', '=', appid)])
-        if account:
-            _set_session_info(account.user_id.id, account.user_id.login)
-            return {'success': 'You have been logged in successfully!', 'session': _get_session_info()}
+    @http.route('/wechat_public_account_auth/validate', type='http', auth="none", methods=["GET"])
+    def validate_auth(self, signature, timestamp, nonce, echostr, **kw):
+        token = "guoodoo786723867"  # 请按照公众平台官网\基本配置中信息填写
+        list = [token, timestamp, nonce]
+        list_data = []
+        for data in list:
+            list_data.append(self.to_binary(data))
+        list_data.sort()
+        _delimiter = self.to_binary(b'')
+        str_to_sign = _delimiter.join(list_data)
+        hashcode = hashlib.sha1(str_to_sign).hexdigest()
+        if hashcode == signature:
+            return echostr
         else:
-            return {'error': 'auth_failed', 'message': 'Authenticating failed, login required.'}
+            return ""
 
-    @http.route('/wechat_mp_auth/unauth_by_code', type='json', auth="user", methods=["POST"])
-    def unauth_by_code(self, code, appid, **kw):
-        ensure_db()
-        try:
-            openid = _get_open_id(code, appid)
-        except RuntimeError as e:
-            return {'error': 'wechat_error', 'message': '%s' % e}
-        account = request.env['wechat.mini.program.account'].sudo().search(
-            [('wechat_open_id', '=', openid), ('wechat_app_id', '=', appid)])
-        if account:
-            account.unlink()
-            return {'success': 'You have been logged out successfully!'}
-        else:
-            return {'error': 'unauth_failed', 'message': 'UnAuthenticating failed'}
+    def to_binary(self, value, encoding='utf-8'):
+        """Convert value to binary string, default encoding is utf-8
+        :param value: Value to be converted
+        :param encoding: Desired encoding
+        """
+        if not value:
+            return b''
+        if isinstance(value, six.binary_type):
+            return value
+        if isinstance(value, six.text_type):
+            return value.encode(encoding)
 
+        return self.to_text(value).encode(encoding)
+
+    def to_text(self, value, encoding='utf-8'):
+        """Convert value to unicode, default encoding is utf-8
+        :param value: Value to be converted
+        :param encoding: Desired encoding
+        """
+        if not value:
+            return ''
+        if isinstance(value, six.text_type):
+            return value
+        if isinstance(value, six.binary_type):
+            return value.decode(encoding)
+
+        return six.text_type(value)
