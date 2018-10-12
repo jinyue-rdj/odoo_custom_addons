6711cfc0cec6ba2882523ed96156490f103b1a69
odoo <guohuanhuan_cool@126.com>
2018-10-12 20:03:49 +0800

    validate wechat public account

---
+++ wechat_public_account_auth/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2018 MAXSNS Corp (http://www.maxsns.com)
+# @author Henry Zhou (zhouhenry@live.com)
+# License OPL-1 - See https://www.odoo.com/documentation/user/11.0/legal/licenses/licenses.html
+{
+    'name': 'Wechat Public Account API',
+    'author': "Huanhuan Guo",
+    'website': "http://www.maxsns.com",
+    'category': 'web',
+    'version': '1.0.0',
+    'depends': [],
+    'data': [
+        'data/config.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+    'license': 'OPL-1',
+}
